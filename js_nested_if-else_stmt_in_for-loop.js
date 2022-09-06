bda13df67dc6338dc30e565955904d141c0bdce0
Rishvanth K S <83532062+iamRishvanth@users.noreply.github.com>
2022-09-06 09:51:41 +0530

    if-else conditional statement inside for loop
    practice challenge for the conditional and looping concept

---
+++ js_nested_if-else_stmt_in_for-loop.js
@@ -0,0 +1,12 @@
+// if-else Conditional statement
+
+let age = [12,17,25];
+for (let i = 0; i < 3; i++){
+  if (age[i] <13){
+    console.log("CHILD");
+  }else if(age[i] <19){
+    console.log("TEEANGER");
+  }else{
+    console.log("ADULT");
+  }
+}
